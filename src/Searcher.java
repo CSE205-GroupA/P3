5cce2a86e98ab4089cc8232a232478cea06a73c4
bbillmeyer <bbillmey@asu.edu>
2021-02-10 16:54:32 -0500

    Completed Searcher, Sorter needs student data to fully complete. Sorting is object based.

---
+++ src/Searcher.java
@@ -0,0 +1,21 @@
+/********************************************************************************************************* 
+ * CLASS: Searcher (Searcher.java) 
+ * 
+ * DESCRIPTION 
+ * Searcher Driver File
+ * 
+ * 
+ * COURSE AND PROJECT INFORMATION 
+ * CSE205 Object Oriented Programming and Data Structures, Spring 2021
+ * Project Number: p3
+ *
+ * GROUP INFORMATION  
+ * AUTHOR 1: Brandon Murata, bmurata1, bmurata1@asu.edu
+ * AUTHOR 2: Brandon Billmeyer, bbillmey , bbillmey@asu.edu
+ * AUTHOR 3: Delaney Claussen , djclaus1, djclaus1@asu.edu
+ * AUTHOR 4: Taylor Hedrick, tmhedric, tmhedric@asu.edu
+ ********************************************************************************************************/
+
+public class Searcher {
+
+}
